2ddeff0a5068b478afb437be8ae092747d31b172
Anass-Dr <anasdrissi77@gmail.com>
2024-10-06 15:08:19 +0100

    JAF-11 added validation for confirm-password and otp

--- src/utils/inputValidate.js
+++ src/utils/inputValidate.js
@@ -1,4 +1,4 @@
-import { object, string } from 'yup';
+import { object, string, ref } from 'yup';
 
 const schemas = {
     username: string().required().min(3).max(20),
@@ -7,8 +7,10 @@ const schemas = {
         .required()
         .min(8)
         .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/, 'Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, and one number'),
+    confirmPassword: string().required().oneOf([ref('password'), null], 'Passwords must match'),
     phone: string().required().min(10).max(10).matches(/^[0-9]+$/, 'Phone number must be a valid 10-digit number'),
     address: string().required(),
+    otp: string().required().min(4).max(4).matches(/^[0-9]+$/, 'OTP must be a valid 4-digit number'),
 }
 
 const genSchema = (inputs) => {
